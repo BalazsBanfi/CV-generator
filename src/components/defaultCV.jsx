1777d84ce2159b21cf37738c76b5e201ac1727d1
BalazsBanfi <banfibalazs81@gmail.com>
2023-12-11 14:42:30 +0100

    Add default Person

---
+++ src/components/defaultCV.jsx
@@ -0,0 +1,27 @@
+export const Person = {
+  personal: {
+    firstname: "Balázs",
+    lastname: "Bánfi",
+    email: "banfibalazs81@gmail.com",
+    phone: "+36701234567",
+    city: "Szeged, HUN",
+  },
+  education: [
+    {
+      degree: "BSC",
+      school: "ZMNE",
+      city: "Budapest",
+      start: "19990801",
+      end: "20030801",
+    },
+  ],
+  profession: [
+    {
+      title: "",
+      company: "IT Solutions",
+      start: "19990801",
+      end: "20030801",
+      description: "I was the leader of the IT team.",
+    },
+  ],
+};
