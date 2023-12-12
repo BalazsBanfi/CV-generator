0474e20acbdfb4fcaec6295c6b8b0a84fd26063e
BalazsBanfi <banfibalazs81@gmail.com>
2023-12-12 08:01:16 +0100

    Organize file header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Editor from "./components/Editor";
 import Printable from "./components/Printable";
 import { Person } from "./components/sampleCV";
-import { useState } from "react";
 
 function App() {
   const [data, setData] = useState(Person);
