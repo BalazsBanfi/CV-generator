32a18f8ec74686c6dbfd982a6bbb8c1ff374d331
bally4h <banfibalazs81@gmail.com>
2023-12-22 15:45:13 +0100

    Add prof and edu func

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,8 +6,16 @@ export default function Editor({
   setEducation,
   setProfession,
 }) {
-  function handleChange(e) {
+  function handleChangePer(e) {
     setPerson({ ...person, [e.target.name]: e.target.value });
+  }
+
+  function handleChangeEd(e) {
+    setEducation({ ...education, [e.target.name]: e.target.value });
+  }
+
+  function handleChangePro(e) {
+    setProfession({ ...profession, [e.target.name]: e.target.value });
   }
 
   return (
@@ -20,7 +28,7 @@ export default function Editor({
             name="firstName"
             placeholder={person.firstName}
             required
-            onChange={handleChange}
+            onChange={handleChangePer}
           ></input>
         </label>
         <label>
@@ -30,7 +38,7 @@ export default function Editor({
             name="lastName"
             placeholder={person.lastName}
             required
-            onChange={handleChange}
+            onChange={handleChangePer}
           ></input>
         </label>
         <label>
@@ -40,7 +48,7 @@ export default function Editor({
             name="city"
             placeholder={person.city}
             required
-            onChange={handleChange}
+            onChange={handleChangePer}
           ></input>
         </label>
         <label>
@@ -50,7 +58,7 @@ export default function Editor({
             name="email"
             placeholder={person.email}
             required
-            onChange={handleChange}
+            onChange={handleChangePer}
           ></input>
         </label>
         <label>
@@ -60,7 +68,7 @@ export default function Editor({
             name="phoneNumber"
             placeholder={person.phoneNumber}
             required
-            onChange={handleChange}
+            onChange={handleChangePer}
           ></input>
         </label>
         <label>
@@ -70,10 +78,129 @@ export default function Editor({
             name="website"
             placeholder={person.website}
             required
-            onChange={handleChange}
+            onChange={handleChangePer}
           ></input>
         </label>
       </form>
+
+      <>
+        {education.map((edu, index) => {
+          return (
+            <form key={edu.id}>
+              <h4>Education {index}.</h4>
+              <label>
+                Degree:
+                <input
+                  type="text"
+                  name="degree"
+                  placeholder={edu.degree}
+                  required
+                  onChange={handleChangeEd}
+                ></input>
+              </label>
+              <label>
+                School:
+                <input
+                  type="text"
+                  name="school"
+                  placeholder={edu.school}
+                  required
+                  onChange={handleChangeEd}
+                ></input>
+              </label>
+              <label>
+                City:
+                <input
+                  type="text"
+                  name="city"
+                  placeholder={edu.city}
+                  required
+                  onChange={handleChangeEd}
+                ></input>
+              </label>
+              <label>
+                Start:
+                <input
+                  type="text"
+                  name="start"
+                  placeholder={edu.start}
+                  required
+                  onChange={handleChangeEd}
+                ></input>
+              </label>
+              <label>
+                End:
+                <input
+                  type="text"
+                  name="end"
+                  placeholder={edu.end}
+                  required
+                  onChange={handleChangeEd}
+                ></input>
+              </label>
+            </form>
+          );
+        })}
+      </>
+      <>
+        {profession.map((prof, index) => {
+          return (
+            <form key={prof.id}>
+              <h4>Profession {index}.</h4>
+              <label>
+                Title:
+                <input
+                  type="text"
+                  name="title"
+                  placeholder={prof.title}
+                  required
+                  onChange={handleChangePro}
+                ></input>
+              </label>
+              <label>
+                Company:
+                <input
+                  type="text"
+                  name="company"
+                  placeholder={prof.company}
+                  required
+                  onChange={handleChangePro}
+                ></input>
+              </label>
+              <label>
+                Description:
+                <input
+                  type="textarea"
+                  name="description"
+                  placeholder={prof.description}
+                  required
+                  onChange={handleChangePro}
+                ></input>
+              </label>
+              <label>
+                Start:
+                <input
+                  type="text"
+                  name="start"
+                  placeholder={prof.start}
+                  required
+                  onChange={handleChangePro}
+                ></input>
+              </label>
+              <label>
+                End:
+                <input
+                  type="text"
+                  name="end"
+                  placeholder={prof.end}
+                  required
+                  onChange={handleChangePro}
+                ></input>
+              </label>
+            </form>
+          );
+        })}
+      </>
     </div>
   );
 }
